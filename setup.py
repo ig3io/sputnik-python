9aa039b70dc731f1fec3a89385fc962539085914
Ignacio Contreras <ignacio@nachovia.com>
2013-02-24 15:08:20 +0100

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sputnik',
+      version='0.0.0',
+      description='Spotify Search API Wrapper',
+      author='Ignacio Contreras Pinilla',
+      author_email='ignacio@nachovia.com',
+      url='http://nachovia.com',
+      packages=['sputnik'],
+     )
