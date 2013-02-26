3f040330c0676abe7ecf8694aca9a60672a05fd0
Ignacio Contreras <ignacio@nachovia.com>
2013-02-26 22:52:22 +0100

    Adds basic unittest

---
+++ test/basic.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+import sputnik
+
+class BasicTest(unittest.TestCase):
+
+    def setUp(self):
+        self.requests_result = requests.get(
+                'http://ws.spotify.com/search/1'
+                '/track.json?q=nine+inch+nails+broken'
+                ).json()
+        self.sputnik_result = sputnik.search('nine inch nails broken')
+        
+
+    def test_number_of_results(self):
+        req_num_results = self.requests_result['info']['num_results']
+        spu_num_results = self.sputnik_result.info['num_results']
+        self.assertEquals(req_num_results, spu_num_results)
+
+
+if __name__ == '__main__':
+    unittest.main()
