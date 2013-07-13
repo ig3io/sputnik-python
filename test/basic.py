a5dfa4caaa2ff28fc486ddb7c54155aed99e6f52
ignaciocontreras <ignacio@ignacio.cat>
2013-07-14 01:46:25 +0200

    Fix error (typo) in test/basic.py

--- test/basic.py
+++ test/basic.py
@@ -9,37 +9,36 @@ class BasicTest(unittest.TestCase):
 
 
     def test_number_of_results(self):
-        requests_result = requests.get(
+        requests_res = requests.get(
                 'http://ws.spotify.com/search/1'
                 '/track.json?q=nine+inch+nails+broken'
                 ).json()
-        sputnik_result = sputnik.search('nine inch nails broken')
+        sputnik_res = sputnik.search('nine inch nails broken')
 
-        req_num_results = requests_result['info']['num_results']
-        spu_num_results = sputnik_result['info']['num_results']
+        req_num = requests_res['info']['num_results']
+        spu_num = sputnik_res['info']['num_results']
 
-        self.assertEquals(req_num_results, spu_num_results,
-                          "raw request and sputnik should return the same "
-                          "number of results")
+        self.assertEquals(req_num, spu_num, "raw request and sputnik should "
+                          "return the same number of results")
 
     def test_default_search_track(self):
         res = sputnik.search("echoplex")
-        tracks = res.get("tracks")
+        tracks = res.get("tracks", None)
         self.assertTrue(tracks is not None)
 
     def test_search_method_track(self):
         res = sputnik.search("the fragile", method="track")
-        tracks = res.get("track")
+        tracks = res.get("tracks", None)
         self.assertTrue(tracks is not None)
 
     def test_search_method_album(self):
         res = sputnik.search("the fragile", method="album")
-        albums = res.get("albums")
+        albums = res.get("albums", None)
         self.assertTrue(albums is not None)
 
     def test_search_method_artist(self):
         res = sputnik.search("david bowie", method="artist")
-        artists = res.get("artists")
+        artists = res.get("artists", None)
         self.assertTrue(artists is not None)
 
 
