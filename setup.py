17e6991934e619c10bcc60a2bcc5f117ba639eee
ignaciocontreras <ignacio@ignacio.cat>
2013-07-14 01:49:26 +0200

    PEP8-fy setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='sputnik',
-      version='0.0.1',
-      description='Spotify Search API Wrapper',
-      author='Ignacio Contreras Pinilla',
-      author_email='ignacio@nachovia.com',
-      url='http://github.com/ignaciocontreras/sputnik',
-      packages=['sputnik'],
-     )
+setup(
+    name='sputnik',
+    version='0.0.1',
+    description='Spotify Search API Wrapper',
+    author='Ignacio Contreras Pinilla',
+    author_email='ignacio@nachovia.com',
+    url='http://github.com/ignaciocontreras/sputnik',
+    packages=['sputnik']
+)
